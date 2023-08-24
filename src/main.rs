de2e783ad0b16dd1912c0fa04c00af11a3d96877
Noor Q <65061866+Mehauk@users.noreply.github.com>
2023-08-23 21:00:53 -0600

    added datasets

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 mod linear_algebra;
+mod machine_learning;
+
+use std::fs::File;
 
 use linear_algebra::matrix;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
+    let s = std::fs::read("src/assets/r.txt")?;
+
+    println!("{:?}", s);
+
+    Ok(())
+}
+
+fn test_custom_matrix_methods() {
     let i = 7;
     let m = matrix::CustomMatrix::random(i, i, -10.0, 10.0);
     let mi = m.copy().invert().unwrap();

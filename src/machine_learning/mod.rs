041bfd7d4e6fb9931839af11dea01c03d06f4d2f
Noor Q <65061866+Mehauk@users.noreply.github.com>
2023-08-30 14:51:26 -0600

    normalized outputs and added biases

--- src/machine_learning/mod.rs
+++ src/machine_learning/mod.rs
@@ -12,25 +12,33 @@ pub mod dataset;
 /// - `hidden_layer` - An arbitrary number of `LxL` weight matrices used in calculating nodes for the next layer.
 /// - `imput_matrix` - A `OxL` matrix that holds the wights used in calculating the output nodes from the hidden layer.
 struct NueralNetwork<const I: usize, const L: usize, const O: usize> {
-    input_matrix: SMatrix<f64, L, I>,
-    hidden_layer: Vec<SMatrix<f64, L, L>>,
-    output_matrix: SMatrix<f64, O, L>,
+    input_matrix: (SMatrix<f64, L, I>, SMatrix<f64, L, 1>),
+    hidden_layer: Vec<(SMatrix<f64, L, L>, SMatrix<f64, L, 1>)>,
+    output_matrix: (SMatrix<f64, O, L>, SMatrix<f64, O, 1>),
 }
 
 impl<const I: usize, const L: usize, const O: usize> NueralNetwork<I, L, O> {
-    pub fn pass_through(&self, input: SMatrix<f64, I, 1>) {
+    pub fn pass_through(&self, input: SMatrix<f64, I, 1>) -> SMatrix<f64, O, 1> {
         // construct the first layer of nodes to start the forward propagation.
         // LxI * Ix1 => Lx1
-        let mut propagating_nodes = self.input_matrix * input;
+        let mut propagating_nodes = self.input_matrix.0 * input + self.input_matrix.1;
+        propagating_nodes.apply(_arctan);
 
         // propagate through each layer in the hidden_layer
         for matrix in self.hidden_layer.iter() {
             // LxL * Lx1 => Lx1
-            propagating_nodes = matrix * propagating_nodes;
+            propagating_nodes = matrix.0 * propagating_nodes + matrix.1;
+            propagating_nodes.apply(_arctan);
         }
 
         // calculate the resulting outputs
         // OxL * Lx1 => Ox1
-        let output = self.output_matrix * propagating_nodes;
+        let mut output = self.output_matrix.0 * propagating_nodes + self.output_matrix.1;
+        output.apply(_arctan);
+        output
     }
+}
+
+fn _arctan(t: &mut f64) {
+    *t = t.atan() / (std::f64::consts::PI / 2.0);
 }

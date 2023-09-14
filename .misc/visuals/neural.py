36e904ca402054b082524f81cf8593162cedecef
Noor Q <65061866+Mehauk@users.noreply.github.com>
2023-09-14 11:30:04 -0600

    added new image

--- .misc/visuals/neural.py
+++ .misc/visuals/neural.py
@@ -2,18 +2,18 @@ from manim import *
 
 class InputNodeOutput(Scene):
     def construct(self):
-        x = MathTex("x").scale(2).move_to(LEFT*3)
-        a = MathTex("a").scale(1.7).move_to(LEFT*2+UP*0.3)
-        w = MathTex("w").scale(1.7).move_to(LEFT*2+UP*0.3)
-        numa = MathTex("2.7").scale(1.5).move_to(LEFT*2+UP*0.3)
-        line = Line(start=LEFT*3 + RIGHT/3, end=LEFT)
-        node = Circle(1)
-        b = MathTex("b").scale(1.7)
-        n = MathTex("n").scale(1.7)
-        numb = MathTex("1.8").scale(1.5)
-        line2 = Arrow(start=RIGHT, end=RIGHT*3)
-        fx = MathTex("f(x)").scale(2).move_to(RIGHT*4)
+        x = MathTex("x").scale(2).move_to(LEFT*5)
+        w = MathTex("w_0").scale(1.7).move_to(LEFT*4+UP*0.3)
+        line = Line(start=LEFT*5 + RIGHT/3, end=LEFT*3)
+        node = Circle(1).move_to(LEFT*2)
+        b = MathTex("b_0").scale(1.7).move_to(LEFT*2)
+        w1 = MathTex("w_1").scale(1.7).move_to(LEFT*0+UP*0.3)
+        line1 = Line(start=LEFT, end=RIGHT)
+        node1 = Circle(1).move_to(RIGHT*2)
+        b1 = MathTex("b_1").scale(1.7).move_to(RIGHT*2)
+        line2 = Arrow(start=RIGHT*3, end=RIGHT*5)
+        fx = MathTex("f(x)").scale(2).move_to(RIGHT*6)
 
         node.color = TEAL
 
-        self.add(x, w, line, node, b, line2, fx)
+        self.add(x, w, line, node, b, w1, line1, node1, b1, line2, fx)

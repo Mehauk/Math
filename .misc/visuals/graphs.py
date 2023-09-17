53c1e8a314e43371b20f8fe8223d6e49956e3b60
Noor Q <65061866+Mehauk@users.noreply.github.com>
2023-09-17 15:21:58 -0600

    added images to readme

--- .misc/visuals/graphs.py
+++ .misc/visuals/graphs.py
@@ -2,33 +2,26 @@ from manim import *
 
 class GraphFunctionDots(Scene):
     def construct(self):
-        plane = NumberPlane(
-            background_line_style={"stroke_opacity": "0.1"}, 
-            x_range=(0, 40, 1),
-            y_range=(0, 30, 1),
-            )
-
         dots = []
-        for i in range(-3, 5):
-            c = TEAL
-            if i < 0 or i > 3: c = GRAY
-            dots.append(Dot(point=i*(UP + RIGHT) + DOWN, color=c))
+        lines = []
+        for i in range(-3, 3):
+            dots.append(Dot(point=i*i*UP + i*RIGHT + 2*DOWN))
+            x = i + 1
+            lines.append(Line(
+                start=i*i*UP + i*RIGHT + 2*DOWN, 
+                end=x*x*UP + x*RIGHT + 2*DOWN
+                              ))
 
-        plane.move_to(DOWN*10 + LEFT*10)
 
-        self.add(plane, *dots)
-
-class GraphFunction(Scene):
-    def construct(self):
         plane = NumberPlane(
             background_line_style={"stroke_opacity": "0.1"}, 
             x_range=(-40, 40, 1),
             y_range=(-30, 30, 1),
             )
 
-        curve = plane.plot(lambda x: x*x)
+        curve = plane.plot(lambda x: x*x - 2)
         curve.color = TEAL
 
         plane.move_to(DOWN*10 + LEFT*10)
 
-        self.add(plane, curve)
+        self.add(plane, curve, *lines, *dots)

f40d76661cf00fac43ba1251ab5a84aaebd352d0
Noor Q <65061866+Mehauk@users.noreply.github.com>
2023-12-29 08:44:04 -0700

    loading logic

--- src/machine_learning/neural_network/mod.rs
+++ src/machine_learning/neural_network/mod.rs
@@ -60,9 +60,16 @@ impl NeuralNetwork {
     pub fn save(&self, file_path: &str) -> Result<(), io::Error> {
         let mut contents = String::new();
 
+        contents += &self
+            ._shape
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<String>();
+        contents += "\n\n";
+
         for w in self._weigths.iter() {
             contents += &format!(
-                "{}, {} - {}\n",
+                "{},{} - {}\n",
                 w.shape().0,
                 w.shape().1,
                 w.data
@@ -98,28 +105,41 @@ impl NeuralNetwork {
     pub fn load(file_path: &str) -> Result<NeuralNetwork, Box<dyn Error>> {
         let contents = std::str::from_utf8(&std::fs::read(file_path)?)?;
 
-        let c_split = contents.split("\n\n").collect::<Vec<&str>>().into_iter();
-        let w = c_split.next().ok_or("File was not properly formatted, or was empty")?;
-        let b = c_split.next().ok_or("File was not properly formatted, or was empty")?;
+        let c_split = contents.split("\n\n");
+        let shape = c_split
+            .next()
+            .ok_or("File was not properly formatted, or was empty")?;
+        let w = c_split
+            .next()
+            .ok_or("File was not properly formatted, or was empty")?;
+        let b = c_split
+            .next()
+            .ok_or("File was not properly formatted, or was empty")?;
 
-        // split \n
-        // split - 
-        // take shape and make matrix
-        let weigths: Vec<DMatrix<f64>> = w.split(pat)
-            .iter()
-            .zip(&shape[0..length - 1])
-            .map(|(a, b)| DMatrix::<f64>::zeros(*a, *b))
-            .collect();
-        let biases: Vec<DVector<f64>> = shape[1..length]
-            .iter()
-            .map(|a| DVector::<f64>::zeros(*a))
-            .collect();
+        let mut weights: Vec<DMatrix<f64>> = Vec::new();
+        for w_line in w.split("\n") {
+            let iter = w_line.split(" - ");
+            let shape = iter.next().ok_or("Incorrect Format")?.split(",");
+            let vals = iter
+                .next()
+                .ok_or("Incorrect Format")?
+                .split(",")
+                .map(|x| x.parse().unwrap_or_default());
+            weights.push(DMatrix::<f64>::from_iterator(
+                shape.next().ok_or("Incorrect Format")?.parse()?,
+                shape.next().ok_or("Incorrect Format")?.parse()?,
+                vals,
+            ))
+        }
 
-        NeuralNetwork {
-            _weigths: weigths,
+        let mut weights: Vec<DVector<f64>> = Vec::new();
+        for b_line in b.split("\n") {}
+
+        Ok(NeuralNetwork {
+            _weigths: weights,
             _biases: biases,
             _shape: shape,
-        }
+        })
     }
 }
 
